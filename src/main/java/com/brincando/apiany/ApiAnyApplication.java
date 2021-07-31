84a5e18f4f5b2b7b52c1b134b57267a44c41331f
Tiago Pereira <tiago.pereira@hotmart.com>
2021-07-31 14:34:36 -0300

    iniciando projeto spring boot

---
+++ src/main/java/com/brincando/apiany/ApiAnyApplication.java
@@ -0,0 +1,13 @@
+package com.brincando.apiany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAnyApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAnyApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/brincando/apiany/ApiAnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brincando.apiany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAnyApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
